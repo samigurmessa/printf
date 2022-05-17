f52daa567422225c1d1ef76dcc46370d5e8e4393
samigurmessa <samuelgurmesa@duck.com>
2022-05-17 13:57:30 -0700

    commit 3

--- main.h
+++ main.h
@@ -6,6 +6,12 @@
 
 int _printf(const char *format, ...);
 
+/* print_number */
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+
 
 
 
